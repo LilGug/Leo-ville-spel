60364a15dbe1727e8658e037dcd70e953471b186
Skojku <jhansson17@gmail.com>
2022-05-31 17:46:20 +0200

    kuk movement

---
+++ Leo spel/Assets/Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector2 speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed.x = Input.GetAxisRaw("Horizontal");
+        transform.Translate(speed * 10 * Time.deltaTime , 0);
+        if ()
+        {
+            
+        }
+
+        print(transform);
+    }
+}
